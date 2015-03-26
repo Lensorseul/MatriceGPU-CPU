c819858d091cfd0b9e9e8a69fd6db9df6ede1fe9
Toulouze Franck <ftoulouz@scinfe053.univ-pau.fr>
2015-03-26 18:05:01 +0100

    New Version

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ using namespace std;
 
 #define GPU 0
 #define CPU 1
+#define STRASSEN 2
 
-const int leafsize = 1024;
-
+const int leafsize = 512;
+int mode = CPU;
 QCLVector<int> inbuffer_A;
 QCLVector<int> inbuffer_B;
 QCLVector<int> outbuffer;
@@ -18,6 +19,9 @@ QCLVector<int> outbuffer;
 QCLKernel kernel;
 QCLContext context;
 QCLProgram program;
+
+void ikjalgorithmCPU(vector< vector<int> > A,vector< vector<int> > B,vector< vector<int> > &C, int n);
+
 void strassen(vector< vector<int> > &A,
               vector< vector<int> > &B,
               vector< vector<int> > &C, unsigned int tam);
@@ -33,6 +37,7 @@ void subtract(vector< vector<int> > &A,
               vector< vector<int> > &B,
               vector< vector<int> > &C, int tam);
 void printMatrix(vector< vector<int> > matrix);
+
 
 vector<int> aPlat(vector< vector<int> > A){
     vector<int> result;
@@ -77,46 +82,19 @@ void ikjalgorithm(vector< vector<int> > A,
     }
 }
 
-int nextPuissance2(int n) {
-    return pow(2, int(ceil(log2(n))));
-}
-
-void sum(vector< vector<int> > &A,
-         vector< vector<int> > &B,
-         vector< vector<int> > &C, int tam) {
-    int i, j;
-    for (i = 0; i < tam; i++) {
-        for (j = 0; j < tam; j++) {
-            C[i][j] = A[i][j] + B[i][j];
-        }
-    }
-}
+void ikjalgorithmCPU(vector< vector<int> > A,
+                     vector< vector<int> > B,
+                     vector< vector<int> > &C, int n) {
 
 
-void subtract(vector< vector<int> > &A,
-              vector< vector<int> > &B,
-              vector< vector<int> > &C, int tam) {
-    int i, j;
-    for (i = 0; i < tam; i++) {
-        for (j = 0; j < tam; j++) {
-            C[i][j] = A[i][j] - B[i][j];
-        }
-    }
-}
-
-
-void printMatrix(vector< vector<int> > matrix) {
-    for (int i=0; i < matrix.size(); i++) {
-        for (int j=0; j < matrix[i].size(); j++) {
-            if (j != 0) {
-                cout << " ";
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] += A[i][k] * B[k][j];
             }
-            cout << matrix[i][j];
         }
-        cout << endl;
     }
 }
-
 
 void strassenR(vector< vector<int> > &A,
                vector< vector<int> > &B,
@@ -191,6 +169,120 @@ void strassenR(vector< vector<int> > &A,
 }
 
 
+int nextPuissance2(int n) {
+    return pow(2, int(ceil(log2(n))));
+}
+
+void sum(vector< vector<int> > &A,
+         vector< vector<int> > &B,
+         vector< vector<int> > &C, int tam) {
+    int i, j;
+    for (i = 0; i < tam; i++) {
+        for (j = 0; j < tam; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+
+void subtract(vector< vector<int> > &A,
+              vector< vector<int> > &B,
+              vector< vector<int> > &C, int tam) {
+    int i, j;
+    for (i = 0; i < tam; i++) {
+        for (j = 0; j < tam; j++) {
+            C[i][j] = A[i][j] - B[i][j];
+        }
+    }
+}
+
+
+void printMatrix(vector< vector<int> > matrix) {
+    for (int i=0; i < matrix.size(); i++) {
+        for (int j=0; j < matrix[i].size(); j++) {
+            if (j != 0) {
+                cout << " ";
+            }
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+
+
+void strassenRCPU(vector< vector<int> > &A,
+               vector< vector<int> > &B,
+               vector< vector<int> > &C, int tam) {
+    if (tam <= leafsize) {
+        ikjalgorithmCPU(A, B, C, tam);
+        return;
+    }
+    // other cases are treated here:
+    else {
+        int newTam = tam/2;
+        vector<int> inner (newTam,0);
+        vector< vector<int> >
+                a11(newTam,inner), a12(newTam,inner), a21(newTam,inner), a22(newTam,inner),
+                b11(newTam,inner), b12(newTam,inner), b21(newTam,inner), b22(newTam,inner),
+                c11(newTam,inner), c12(newTam,inner), c21(newTam,inner), c22(newTam,inner),
+                p1(newTam,inner), p2(newTam,inner), p3(newTam,inner), p4(newTam,inner),
+                p5(newTam,inner), p6(newTam,inner), p7(newTam,inner),
+                aResult(newTam,inner), bResult(newTam,inner);
+        int i, j;
+        //dividing the matrices in 4 sub-matrices:
+        for (i = 0; i < newTam; i++) {
+            for (j = 0; j < newTam; j++) {
+                a11[i][j] = A[i][j];
+                a12[i][j] = A[i][j + newTam];
+                a21[i][j] = A[i + newTam][j];
+                a22[i][j] = A[i + newTam][j + newTam];
+                b11[i][j] = B[i][j];
+                b12[i][j] = B[i][j + newTam];
+                b21[i][j] = B[i + newTam][j];
+                b22[i][j] = B[i + newTam][j + newTam];
+            }
+        }
+
+        // Calculating p1 to p7:
+        sum(a11, a22, aResult, newTam); // a11 + a22
+        sum(b11, b22, bResult, newTam); // b11 + b22
+        strassenRCPU(aResult, bResult, p1, newTam); // p1 = (a11+a22) * (b11+b22)
+        sum(a21, a22, aResult, newTam); // a21 + a22
+        strassenRCPU(aResult, b11, p2, newTam); // p2 = (a21+a22) * (b11)
+        subtract(b12, b22, bResult, newTam); // b12 - b22
+        strassenRCPU(a11, bResult, p3, newTam); // p3 = (a11) * (b12 - b22)
+        subtract(b21, b11, bResult, newTam); // b21 - b11
+        strassenRCPU(a22, bResult, p4, newTam); // p4 = (a22) * (b21 - b11)
+        sum(a11, a12, aResult, newTam); // a11 + a12
+        strassenRCPU(aResult, b22, p5, newTam); // p5 = (a11+a12) * (b22)
+        subtract(a21, a11, aResult, newTam); // a21 - a11
+        sum(b11, b12, bResult, newTam); // b11 + b12
+        strassenRCPU(aResult, bResult, p6, newTam); // p6 = (a21-a11) * (b11+b12)
+        subtract(a12, a22, aResult, newTam); // a12 - a22
+        sum(b21, b22, bResult, newTam); // b21 + b22
+        strassenRCPU(aResult, bResult, p7, newTam); // p7 = (a12-a22) * (b21+b22)
+        // calculating c21, c21, c11 e c22:
+        sum(p3, p5, c12, newTam); // c12 = p3 + p5
+        sum(p2, p4, c21, newTam); // c21 = p2 + p4
+        sum(p1, p4, aResult, newTam); // p1 + p4
+        sum(aResult, p7, bResult, newTam); // p1 + p4 + p7
+        subtract(bResult, p5, c11, newTam); // c11 = p1 + p4 - p5 + p7
+        sum(p1, p3, aResult, newTam); // p1 + p3
+        sum(aResult, p6, bResult, newTam); // p1 + p3 + p6
+        subtract(bResult, p2, c22, newTam); // c22 = p1 + p3 - p2 + p6
+        // Grouping the results obtained in a single matrix:
+        for (i = 0; i < newTam ; i++) {
+            for (j = 0 ; j < newTam ; j++) {
+                C[i][j] = c11[i][j];
+                C[i][j + newTam] = c12[i][j];
+                C[i + newTam][j] = c21[i][j];
+                C[i + newTam][j + newTam] = c22[i][j];
+            }
+        }
+    }
+}
+
+
 void strassen(vector< vector<int> > &A,
               vector< vector<int> > &B,
               vector< vector<int> > &C, unsigned int n) {
@@ -204,7 +296,12 @@ void strassen(vector< vector<int> > &A,
             BPrep[i][j] = B[i][j];
         }
     }
+
+   if(mode==STRASSEN){
+       strassenRCPU(APrep,BPrep,CPrep, m);
+   }else{
     strassenR(APrep, BPrep, CPrep, m);
+   }
     for(unsigned int i=0; i<n; i++) {
         for (unsigned int j=0; j<n; j++) {
             C[i][j] = CPrep[i][j];
@@ -225,15 +322,14 @@ void strassen(vector< vector<int> > &A,
 
 int main(int argc, char *argv[])
 {    
-
     //dimension des matrices
     int N=10;
-    int mode = CPU;
     if(argv[2] != NULL)
         N=(atoi(argv[2])>0)?atoi(argv[2]):10;
     if(argv[1] != NULL){
         mode = ( strcmp(argv[1],"-cpu") == 0)?CPU:mode;
         mode = ( strcmp(argv[1],"-gpu") == 0)?GPU:mode;
+        mode = ( strcmp(argv[1],"-strassen") == 0)?STRASSEN:mode;
     }
 
 
@@ -250,7 +346,7 @@ int main(int argc, char *argv[])
     }
 
     if (mode == CPU) {
-        cout<<"Multiplication en mode CPU"<<endl;
+        cout<<"Multiplication en mode CPU avec matrice en 1D"<<endl;
 
         clock_t    timer;
         timer = clock();
@@ -265,9 +361,9 @@ int main(int argc, char *argv[])
             }
         }
 
-        cout << "Timer CPU: " << (clock() - timer) / (double)(CLOCKS_PER_SEC / 1000) << " ms" <<endl;
+        cout << "Timer CPU: " << (clock() - timer) / (double)(CLOCKS_PER_SEC)<< " ms" <<endl;
 
-    }else{
+    }else if(mode== GPU){
         if(!context.create()){
             qFatal("Could not create OpenCL context for the GPU\n");
             exit(0);
@@ -287,9 +383,33 @@ int main(int argc, char *argv[])
 
         cout<<"Multiplication en mode GPU"<<endl;
 
+        clock_t    timer;
+        timer = clock();
         strassen(A,B,C,n);
         cout<<"C [0] "<<C[0][0]<<endl;
+        cout << "Timer CPU: " << (clock() - timer) / (double)(CLOCKS_PER_SEC) << " ms" <<endl;
 
+
+    }else if(mode==STRASSEN){
+        cout<<"Multiplication en mode CPU avec algorithme STRASSEN"<<endl;
+
+        int n = N;
+        vector<int> inner (n,0);
+        vector< vector<int> > A(n, inner), B(n, inner), C(n, inner);
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                A[i][j] = B[i][j] = 1;
+            }
+        }
+        if(N != nextPuissance2(N)){
+            N = nextPuissance2(N);
+        }
+
+        clock_t    timer;
+        timer = clock();
+        strassen(A,B,C,n);
+        cout<<"C [0] "<<C[0][0]<<endl;
+        cout << "Timer CPU avec strassen: " << (clock() - timer) / (double)(CLOCKS_PER_SEC) << " ms" <<endl;
 
     }
 
